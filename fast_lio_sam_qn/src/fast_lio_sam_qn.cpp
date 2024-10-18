d593377187b1738eaff8c7cc576b9f5d0a62aad0
Hyungtae Lim <shapelim@mit.edu>
2024-10-18 00:23:14 -0400

    Remove method.cpp

--- fast_lio_sam_qn/src/fast_lio_sam_qn.cpp
+++ fast_lio_sam_qn/src/fast_lio_sam_qn.cpp
@@ -481,3 +481,38 @@ FastLioSamQn::~FastLioSamQn()
     cout << "\033[32;1mResult saved in .pcd format!!!\033[0m" << endl;
   }
 }
+
+void FastLioSamQn::updateVisVars(const PosePcd &pose_pcd_in)
+{
+  m_odoms.points.emplace_back(pose_pcd_in.pose_eig(0, 3), pose_pcd_in.pose_eig(1, 3), pose_pcd_in.pose_eig(2, 3));
+  m_corrected_odoms.points.emplace_back(pose_pcd_in.pose_corrected_eig(0, 3), pose_pcd_in.pose_corrected_eig(1, 3), pose_pcd_in.pose_corrected_eig(2, 3));
+  m_odom_path.poses.push_back(poseEigToPoseStamped(pose_pcd_in.pose_eig, m_map_frame));
+  m_corrected_path.poses.push_back(poseEigToPoseStamped(pose_pcd_in.pose_corrected_eig, m_map_frame));
+  return;
+}
+
+visualization_msgs::Marker FastLioSamQn::getLoopMarkers(const gtsam::Values &corrected_esti_in)
+{
+  visualization_msgs::Marker edges_; edges_.type = 5u;
+  edges_.scale.x = 0.12f; edges_.header.frame_id = m_map_frame; edges_.pose.orientation.w = 1.0f;
+  edges_.color.r = 1.0f; edges_.color.g = 1.0f; edges_.color.b = 1.0f; edges_.color.a = 1.0f;
+  for (int i = 0; i < m_loop_idx_pairs.size(); ++i)
+  {
+    if (m_loop_idx_pairs[i].first >= corrected_esti_in.size() || m_loop_idx_pairs[i].second >= corrected_esti_in.size()) continue;
+    gtsam::Pose3 pose_ = corrected_esti_in.at<gtsam::Pose3>(m_loop_idx_pairs[i].first);
+    gtsam::Pose3 pose2_ = corrected_esti_in.at<gtsam::Pose3>(m_loop_idx_pairs[i].second);
+    geometry_msgs::Point p_, p2_;
+    p_.x = pose_.translation().x(); p_.y = pose_.translation().y(); p_.z = pose_.translation().z();
+    p2_.x = pose2_.translation().x(); p2_.y = pose2_.translation().y(); p2_.z = pose2_.translation().z();
+    edges_.points.push_back(p_);
+    edges_.points.push_back(p2_);
+  }
+  return edges_;
+}
+
+bool FastLioSamQn::checkIfKeyframe(const PosePcd &pose_pcd_in, const PosePcd &latest_pose_pcd)
+{
+  return m_keyframe_thr < (latest_pose_pcd.pose_corrected_eig.block<3, 1>(0, 3) - pose_pcd_in.pose_corrected_eig.block<3, 1>(0, 3)).norm();
+}
+
+
