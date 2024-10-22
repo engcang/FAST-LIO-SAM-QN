a9b2e543ae143861777a85fa5082df99daaedfdf
Hyungtae Lim <shapelim@mit.edu>
2024-10-22 09:50:36 -0400

    Remove spaces and change annotations for consistency

--- fast_lio_sam_qn/src/fast_lio_sam_qn.cpp
+++ fast_lio_sam_qn/src/fast_lio_sam_qn.cpp
@@ -22,12 +22,8 @@ PosePcd::PosePcd(const nav_msgs::Odometry &odom_in, const sensor_msgs::PointClou
 FastLioSamQn::FastLioSamQn(const ros::NodeHandle& n_private) : m_nh(n_private)
 {
   ////// ROS params
-  // temp vars, only used in constructor
-  double loop_update_hz_, vis_hz_;
-
   auto & gc = lc_config_.gicp_config_;
   auto & qc = lc_config_.quatro_config_;
-  // get params
   /* basic */
   m_nh.param<std::string>("/basic/map_frame", m_map_frame, "map");
   m_nh.param<double>("/basic/loop_update_hz", loop_update_hz_, 1.0);
@@ -38,11 +34,9 @@ FastLioSamQn::FastLioSamQn(const ros::NodeHandle& n_private) : m_nh(n_private)
   m_nh.param<double>("/keyframe/keyframe_threshold", m_keyframe_thr, 1.0);
   m_nh.param<int>("/keyframe/num_submap_keyframes", lc_config_.num_submap_keyframes_, 5);
   m_nh.param<bool>("/keyframe/enable_submap_matching", lc_config_.enable_submap_matching_, false);
-  
   /* loop */
   m_nh.param<double>("/loop/loop_detection_radius", lc_config_.loop_detection_radius_, 15.0);
   m_nh.param<double>("/loop/loop_detection_timediff_threshold", lc_config_.loop_detection_timediff_threshold_, 10.0);
-
   /* nano (GICP config) */
   m_nh.param<int>("/nano_gicp/thread_number", gc.nano_thread_number_, 0);
   m_nh.param<double>("/nano_gicp/icp_score_threshold", gc.icp_score_thr_, 10.0);
@@ -52,7 +46,6 @@ FastLioSamQn::FastLioSamQn(const ros::NodeHandle& n_private) : m_nh(n_private)
   m_nh.param<double>("/nano_gicp/euclidean_fitness_epsilon", gc.euclidean_fitness_epsilon_, 0.01);
   m_nh.param<int>("/nano_gicp/ransac/max_iter", gc.nano_ransac_max_iter_, 5);
   m_nh.param<double>("/nano_gicp/ransac/outlier_rejection_threshold", gc.ransac_outlier_rejection_threshold_, 1.0);
-
   /* quatro (Quatro config) */
   m_nh.param<bool>("/quatro/enable", lc_config_.enable_quatro_, false);
   m_nh.param<bool>("/quatro/optimize_matching", qc.use_optimized_matching_, true);
@@ -65,25 +58,20 @@ FastLioSamQn::FastLioSamQn(const ros::NodeHandle& n_private) : m_nh(n_private)
   m_nh.param<double>("/quatro/rotation/gnc_factor", qc.rot_gnc_factor_, 1.4);
   m_nh.param<double>("/quatro/rotation/rot_cost_diff_threshold", qc.rot_cost_diff_thr_, 0.0001);
   m_nh.param<int>("/quatro/rotation/num_max_iter", qc.quatro_max_iter_, 50);
-  
   /* results */
   m_nh.param<bool>("/result/save_map_bag", m_save_map_bag, false);
   m_nh.param<bool>("/result/save_map_pcd", m_save_map_pcd, false);
   m_nh.param<bool>("/result/save_in_kitti_format", m_save_in_kitti_format, false);
   m_nh.param<std::string>("/result/seq_name", m_seq_name, "");
-
   loop_closure_.reset(new LoopClosure(lc_config_));
-  ////// GTSAM init
+  /* Initialization of GTSAM */
   gtsam::ISAM2Params isam_params_;
   isam_params_.relinearizeThreshold = 0.01;
   isam_params_.relinearizeSkip = 1;
   m_isam_handler = std::make_shared<gtsam::ISAM2>(isam_params_);
-
-  ////// ROS things
   m_odom_path.header.frame_id = m_map_frame;
   m_corrected_path.header.frame_id = m_map_frame;
   m_package_path = ros::package::getPath("fast_lio_sam_qn");
-  // publishers
   m_odom_pub = m_nh.advertise<sensor_msgs::PointCloud2>("/ori_odom", 10, true);
   m_path_pub = m_nh.advertise<nav_msgs::Path>("/ori_path", 10, true);
   m_corrected_odom_pub = m_nh.advertise<sensor_msgs::PointCloud2>("/corrected_odom", 10, true);
@@ -96,16 +84,18 @@ FastLioSamQn::FastLioSamQn(const ros::NodeHandle& n_private) : m_nh(n_private)
   m_debug_dst_pub = m_nh.advertise<sensor_msgs::PointCloud2>("/dst", 10, true);
   m_debug_coarse_aligned_pub = m_nh.advertise<sensor_msgs::PointCloud2>("/coarse_aligned_quatro", 10, true);
   m_debug_fine_aligned_pub = m_nh.advertise<sensor_msgs::PointCloud2>("/fine_aligned_nano_gicp", 10, true);
-  // subscribers
   m_sub_odom = std::make_shared<message_filters::Subscriber<nav_msgs::Odometry>>(m_nh, "/Odometry", 10);
   m_sub_pcd = std::make_shared<message_filters::Subscriber<sensor_msgs::PointCloud2>>(m_nh, "/cloud_registered", 10);
   m_sub_odom_pcd_sync = std::make_shared<message_filters::Synchronizer<odom_pcd_sync_pol>>(odom_pcd_sync_pol(10), *m_sub_odom, *m_sub_pcd);
   m_sub_odom_pcd_sync->registerCallback(boost::bind(&FastLioSamQn::odomPcdCallback, this, _1, _2));
   m_sub_save_flag = m_nh.subscribe("/save_dir", 1, &FastLioSamQn::saveFlagCallback, this);
-  // Timers at the end
   m_loop_timer = m_nh.createTimer(ros::Duration(1/loop_update_hz_), &FastLioSamQn::loopTimerFunc, this);
   m_vis_timer = m_nh.createTimer(ros::Duration(1/vis_hz_), &FastLioSamQn::visTimerFunc, this);
 
+  /* ROS things */
+  /* publishers */
+  /* subscribers */
+  /* Timers */
   ROS_WARN("Main class, starting node...");
 }
 
