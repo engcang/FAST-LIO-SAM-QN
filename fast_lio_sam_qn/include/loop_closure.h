da274beb6ab75515e89df97ffa2443d5021d8526
Hyungtae Lim <shapelim@mit.edu>
2024-10-22 09:46:16 -0400

    Remove spaces between function declarations

--- fast_lio_sam_qn/include/loop_closure.h
+++ fast_lio_sam_qn/include/loop_closure.h
@@ -74,25 +74,15 @@ class LoopClosure
 
     LoopClosure(const LoopClosureConfig &config);
     ~LoopClosure();
-
     int fetchClosestKeyframeIdx(const PosePcd &query_keyframe, const std::vector<PosePcd> &keyframes);
-
     std::tuple<pcl::PointCloud<PointType>, pcl::PointCloud<PointType>> setSrcAndDstCloud(std::vector<PosePcd> keyframes, const int src_idx, const int dst_idx, const int submap_range, const double voxel_res, const bool enable_quatro, const bool enable_submap_matching);
-
     RegistrationOutput icpAlignment(const pcl::PointCloud<PointType> &src, const pcl::PointCloud<PointType> &dst);
-
     RegistrationOutput coarseToFineAlignment(const pcl::PointCloud<PointType> &src, const pcl::PointCloud<PointType> &dst);
-
     RegistrationOutput retrieveLoopClosureMeasurement(const PosePcd &query_keyframe, const std::vector<PosePcd> &keyframes);
-
     pcl::PointCloud<PointType> getSourceCloud();
-
     pcl::PointCloud<PointType> getTargetCloud();
-
     pcl::PointCloud<PointType> getCoarseAlignedCloud();
-
     pcl::PointCloud<PointType> getFinalAlignedCloud();
-
     int getClosestKeyframeidx();
 };
 
